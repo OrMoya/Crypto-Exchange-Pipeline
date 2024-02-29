5fbb9db291ea57cacbe3c9c0b863d157724562ab
Orlando Moya <orlandomoyarodriguez@gmail.com>
2024-02-29 14:33:26 -0800

    merge changes in etl_load

--- src/cryptocoindatapipline/etl_load.py
+++ src/cryptocoindatapipline/etl_load.py
@@ -1,9 +1,6 @@
 import json 
 import boto3
-<<<<<<< HEAD
 import botocore
-=======
->>>>>>> 2e555c5c8c0ecbded4ac34d8b98fa77ee9f05dc6
 import psycopg2.extras
 
 from util.db import DatabaseConnection
@@ -17,13 +14,8 @@ key_name = get_most_recent_s3_object('crypto-data-raw', 'crypto_data_raw')['Key'
 
 def exchange_insert_query() -> str:
     return  '''
-<<<<<<< HEAD
     INSERT INTO public.exchanges (
         coinid,
-=======
-    INSERT INTO public.exchanges_test5 (
-        id,
->>>>>>> 2e555c5c8c0ecbded4ac34d8b98fa77ee9f05dc6
         name,
         rank,
         percenttotalvolume,
@@ -32,12 +24,8 @@ def exchange_insert_query() -> str:
         socket,
         exchangeurl,
         updated_unix_millis,
-<<<<<<< HEAD
         updated_utc,
         upload_utc
-=======
-        updated_utc
->>>>>>> 2e555c5c8c0ecbded4ac34d8b98fa77ee9f05dc6
     )
     VALUES (
         %(exchangeId)s,
@@ -49,12 +37,8 @@ def exchange_insert_query() -> str:
         %(socket)s,
         %(exchangeUrl)s,
         %(updated)s,
-<<<<<<< HEAD
         %(update_dt)s,
         now()::timestamp
-=======
-        %(update_dt)s
->>>>>>> 2e555c5c8c0ecbded4ac34d8b98fa77ee9f05dc6
     );
 '''
 
@@ -72,8 +56,3 @@ def run() -> None:
     with DatabaseConnection(get_database_creds()).managed_cursor() as curr:
         psycopg2.extras.execute_batch(curr, exchange_insert_query(), data)
 
-<<<<<<< HEAD
-=======
-if __name__ == "__main__":
-    run()
->>>>>>> 2e555c5c8c0ecbded4ac34d8b98fa77ee9f05dc6
