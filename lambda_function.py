7ed0c210d3233e4dfb72f4d25be6c35e727baa09
Orlando Moya <orlandomoyarodriguez@gmail.com>
2024-03-08 14:38:33 -0800

    adjusted lambda_function.py to include json reponses

--- lambda_function.py
+++ lambda_function.py
@@ -4,9 +4,16 @@ from cryptocoindatapipeline.etl_extract import extract_run
 from cryptocoindatapipeline.etl_load import load_run
 
 def lambda_handler(event, context):
-    etl_extract.run()
-    etl_load.run()
-    return {
-        'statusCode': 400,
-        'body': json.dumps('Error: ETL Pipeline did not run!')
-    }
+    try:
+        etl_extract.run()
+        etl_load.run()
+        return {
+            'statusCode': 200,
+            'body': json.dumps('sucess!')
+        }
+    except:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Error: ETL Pipeline did not run!')
+        }
+        
